25e6b63c482eaa9af8332fc619c3453c839584d8
kilvk <afinni6254@gmail.com>
2025-10-10 18:09:41 +0100

    Changed Stressbar Script
    Seperated timer from slider controls

--- Assets/Scripts/Stressbar.cs
+++ Assets/Scripts/Stressbar.cs
@@ -6,35 +6,61 @@ public class Stressbar : MonoBehaviour
     // Assignable Slider Values
     [SerializeField] private Slider timerSlider; // The Slider object
     [SerializeField] private Text timerText; // The Text object
+    [SerializeField] private Text resultText; // The Result Text object
     [SerializeField] private float gameTime; // time for the minigame
-
-    private bool fishCaught;
+    private float catchBar; // the actual stress bar
+    private bool fishCaught; // boolean saying the fish is caught
+    private bool gameEnd; // boolean stating the game is over
     void Start()
     {
         fishCaught = false;
-        timerSlider.maxValue = gameTime;
-        timerSlider.value = gameTime;
+        catchBar = gameTime;
+        timerSlider.maxValue = catchBar;
+        timerSlider.value = catchBar;
     }
 
     // Update is called once per frame
     void Update()
     {
         float time = gameTime - Time.time;
+        catchBar = catchBar - Time.time;
 
         int minutes = Mathf.FloorToInt(time / 60);
         int seconds = Mathf.FloorToInt(time - minutes * 60);
 
         string textTime = string.Format("{0:0}:{1:00}", minutes, seconds);
+        string resText = "";
 
-        if (time <= 0)
-        {
-            fishCaught = true;
-        }
-
-        if (fishCaught == false)
+        // Checks if the game is over
+        if (gameEnd == false)
         {
             timerText.text = textTime;
+            resultText.text = resText;
             timerSlider.value = time;
+        }
+
+        // Checks the time
+        if (time <= 0)
+        {
+            // checks if the catch bar is empty
+            if (catchBar <= 0)
+            {
+                fishCaught = false;
+            }
+            else
+            {
+                fishCaught = true;
+            }
+
+            if (fishCaught == false)
+            {
+                resText = "Escaped!";
+            }
+            else
+            {
+                resText = "Caught!";
+            }
+            gameEnd = true;
         }
     }
 }

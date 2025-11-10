ba9c7deef6e2a353410b45706ea11f1ce9bfc7c6
Callum <ckelly1536@outlook.com>
2025-11-10 19:12:59 +0000

    Pause Manager And Clean-Ups
    Removed the 'RememberSelectedButton' script since it was redundant
    Renamed the 'Main Menu' script to 'SceneLoader'
    Removed redundant code from SceneLoader, since a lot of the functionality can be done in the inspector
    Fixed the main menu buttons after script alterations
    Created the pause menu on the level scene with UI navigation

---
+++ Assets/Programming/UI/Pause Menu/PauseManager.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseManager : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public Button buttonToSelect;
+    private PlayerMovement player;
+    private bool isPaused;
+
+    private void Start()
+    {
+        player = FindFirstObjectByType<PlayerMovement>();
+        pauseMenu.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P) && !isPaused)
+        {
+            OnPause();
+        }
+        else if (Input.GetKeyDown(KeyCode.P) && isPaused)
+        {
+            OnResume();
+        }
+    }
+    public void OnPause()
+    {
+        buttonToSelect.Select();
+        player.enabled = false;
+        isPaused = true;
+        Time.timeScale = 0f;
+        pauseMenu.SetActive(true);
+    }
+
+    public void OnResume()
+    {
+        player.enabled = true;
+        isPaused = false;
+        Time.timeScale = 1f;
+        pauseMenu.SetActive(false);
+    }
+}
